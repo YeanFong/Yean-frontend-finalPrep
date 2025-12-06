d2d6e90b9c63311a89958c15a5bb4d7301dc9726
YeanFong <2024307fong@aupp.edu.kh>
2025-12-06 13:03:00 +0700

    update2

---
+++ my-app/src/index.js
@@ -0,0 +1,7 @@
+import { quiz6 } from "./quizzes/quiz6";
+import { quiz7 } from "./quizzes/quiz7";
+import { quiz8 } from "./quizzes/quiz8";
+import { quiz9 } from "./quizzes/quiz9";
+import { quiz10 } from "./quizzes/quiz10";
+
+export const QUIZZES = [quiz6, quiz7, quiz8, quiz9, quiz10];
--- my-app/src/quizzes/index.js
+++
@@ -1,7 +0,0 @@
-import { quiz6 } from "./quiz6";
-import { quiz7 } from "./quiz7";
-import { quiz8 } from "./quiz8";
-import { quiz9 } from "./quiz9";
-import { quiz10 } from "./quiz10";
-
-export const QUIZZES = [quiz6, quiz7, quiz8, quiz9, quiz10];
